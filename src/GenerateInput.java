bd302128c493a48a36a16cb75f7c57439c2837c6
ZERUI GONG <Nomlasssssss@outlook.com>
2022-09-08 13:42:30 +0800

    added GenerateInput implementation

--- src/GenerateInput.java
+++ src/GenerateInput.java
@@ -1,3 +1,25 @@
+
+/*
+ * Generate arrays of increasing sizes, in a range from 1,000 to 10 million. 
+ * For each of the sizes, generate a random dataset of integers
+ * in the range of [1, …, x], where x is the largest number you allow for your datasets.
+ */
+import java.util.*;
+
 public class GenerateInput {
-    
+    public static void main(String[] args) {
+        int[] testArr = generateRandom(10, 100);
+        System.out.println(Arrays.toString(testArr));
+
+    }
+
+    public static int[] generateRandom(int size, int max) {
+        int[] arr = new int[size];
+        for (int i = 0; i < arr.length; i++)
+            /*java random returns a pseudorandom,
+            // uniformly distributed int value between 0 (inclusive) and the specified value (exclusive)*/
+            arr[i] = new Random().nextInt(max-1+1)+1; 
+        return arr;
+    }
+
 }
