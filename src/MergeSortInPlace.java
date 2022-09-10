0fc384e6b9a236fc0698ea6f77cc765a88a10d80
ZERUI GONG <Nomlasssssss@outlook.com>
2022-09-10 16:24:14 +0800

    <fix> fixed bugs in mergeSortInplace

--- src/MergeSortInPlace.java
+++ src/MergeSortInPlace.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class MergeSortInPlace {
     public static void main(String[] args) {
-        int[] testArr = { 3, 5, 7, 8, 4, 6, 7, 9 };
+        int[] testArr = { 3,99,5 };
         sortIt(testArr, 0, testArr.length - 1);
         System.out.println(Arrays.toString(testArr));
     }
@@ -11,8 +11,12 @@ public class MergeSortInPlace {
         int mid = (start + end) / 2;
 
         if ((mid - start) <= 0)
+        {
+            if(arr[start] > arr[end])
+                swap(arr,start,end);
             return;
-        else if ((mid - start) > 1) {
+        }
+        else if ((end - start) > 1) {
             sortIt(arr, start, mid);
             sortIt(arr, mid + 1, end);
         }
@@ -23,31 +27,25 @@ public class MergeSortInPlace {
     public static void merge(int[] arr, int start, int end) {
         int mid = (start + end) / 2;
         int a = start, b = mid + 1;
-        int temp;
+        int temp,i;
 
         if ((end - start) <= 0)
             return;
         while (a <= mid && b <= end) {
             if (arr[a] < arr[b]) {
                 a++;
-            } else if (arr[a] > arr[b]) {
-                temp = arr[b++];
-                for (int i = ++mid; i > a; i--) {
-                    arr[i] = arr[i - 1];
-                    arr[a++] = temp;
-                }
             } else {
-                if (a == mid && b == end) {
-                    break;
+                temp = arr[b];
+                for(i = b;i>a;i--){
+                    arr[i] = arr[i-1];
                 }
-                temp = arr[b++];
-                a++;
-                for (int i = ++mid; i > a; i--) {
-                    arr[i] = arr[i - 1];
-                }
-                arr[a++] = temp;
+                arr[a] = temp;
+                a++;mid++;b++;
             }
         }
 
+    }
+    public static void swap(int[] arr, int i,int j){
+        int temp = arr[i]; arr[i] = arr[j]; arr[j] = temp;
     }
 }
