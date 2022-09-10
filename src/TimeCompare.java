0bc67391c916fac080c9bafa075f50a9ec1acbc2
Joel David Lim <72362709+joedav98@users.noreply.github.com>
2022-09-10 17:52:28 +0800

    Update to compareTime
    Modularised the different sorts

--- src/TimeCompare.java
+++ src/TimeCompare.java
@@ -1,6 +1,34 @@
 import java.util.Arrays;
 
 public class TimeCompare {
+
+    public static long mergeInplace(int[] testArrInplace){
+        long begin, end;
+        begin = System.nanoTime();
+        MergeSortInPlace.sortIt(testArrInplace,0,testArrInplace.length-1);
+        //end = System.currentTimeMillis();
+        end = System.nanoTime();
+        return end-begin;
+    }
+
+    public static long mergeOriginal(int[] testArrOriginal){
+        long begin, end;
+        begin = System.nanoTime();
+        MergeSortOriginal.sortIt(testArrOriginal,0,testArrOriginal.length-1);
+        //end = System.currentTimeMillis();
+        end = System.nanoTime();
+        return end-begin;
+    }
+
+    public static long insertion(int[] testArrInsert){
+        long begin, end;
+        begin = System.nanoTime();
+        InsertionSort.sortIt(testArrInsert,0,testArrInsert.length-1);
+        //end = System.currentTimeMillis();
+        end = System.nanoTime();
+        return end-begin;
+    }
+
     public static void main(String[] args) {
         // 3 copies of the same array
         int[] testArr1 = GenerateInput.generateRandom(30, 100);
@@ -8,28 +36,11 @@ public class TimeCompare {
         int[] testArr3 = Arrays.copyOf(testArr1, testArr1.length);
 
         System.arraycopy(testArr1, 0, testArr2,0, testArr1.length);
-        long begin, end, mergeTime1, mergeTime2, insertTime;
+        long mergeTime1, mergeTime2, insertTime;
 
-        //begin = System.currentTimeMillis();
-        begin = System.nanoTime();
-        MergeSortInPlace.sortIt(testArr1,0,testArr1.length-1);
-        //end = System.currentTimeMillis();
-        end = System.nanoTime();
-        mergeTime1 = end-begin;
-
-        //begin = System.currentTimeMillis();
-        begin = System.nanoTime();
-        MergeSortOriginal.sortIt(testArr2,0,testArr2.length-1);
-        //end = System.currentTimeMillis();
-        end = System.nanoTime();
-        mergeTime2 = end-begin;
-
-        //begin = System.currentTimeMillis();
-        begin = System.nanoTime();
-        InsertionSort.sortIt(testArr3,0,testArr3.length-1);
-        //end = System.currentTimeMillis();
-        end = System.nanoTime();
-        insertTime = end-begin;
+        mergeTime1 = mergeInplace(testArr1);
+        mergeTime2 = mergeOriginal(testArr2);
+        insertTime = insertion(testArr3);
 
         System.out.println("Time for MergeSort(Inplace) : "+ mergeTime1 + " ns");
         System.out.println("Time for MergeSort(Orignal) : "+ mergeTime2 + " ns");
