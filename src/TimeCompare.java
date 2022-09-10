d1958ac6cecc0288d90c0029ba1b09de7b2d3aa8
Joel David Lim <72362709+joedav98@users.noreply.github.com>
2022-09-10 19:24:20 +0800

    Update to TimeCompare
    Added a constant for MAX and ITER. ITER being the number of iterations each algo would take to average.

--- src/TimeCompare.java
+++ src/TimeCompare.java
@@ -1,64 +1,92 @@
 
 
 public class TimeCompare {
+    static final int MAX = 100;
+    static final int ITER = 30;
 
-    public static long mergeInplace(int[] testArrInplace){
-        long begin, end;
-        begin = System.currentTimeMillis();
-        MergeSortInPlace.sortIt(testArrInplace,0,testArrInplace.length-1);
-        //end = System.currentTimeMillis();
-        end = System.currentTimeMillis();
-        return end-begin;
+    public static long mergeInplace(int size){
+        int[] testArrInplace;
+        long total = 0;
+
+        for(int i = 0; i < ITER; i++){
+            testArrInplace = GenerateInput.generateRandom(size, MAX);
+            long begin, end;
+            begin = System.currentTimeMillis();
+            MergeSortInPlace.sortIt(testArrInplace,0,testArrInplace.length-1);
+            end = System.currentTimeMillis();
+
+            total += (end - begin);
+        }
+        
+
+        return total/ITER;
     }
 
-    public static long mergeOriginal(int[] testArrOriginal){
-        long begin, end;
-        begin = System.currentTimeMillis();
-        MergeSortOriginal.sortIt(testArrOriginal,0,testArrOriginal.length-1);
-        //end = System.currentTimeMillis();
-        end = System.currentTimeMillis();
-        return end-begin;
+    public static long mergeOriginal(int size){
+        int[] testArrOriginal; 
+        long total = 0;
+        
+        for(int i = 0; i < ITER; i++){
+            testArrOriginal = GenerateInput.generateRandom(size, MAX);
+            long begin, end;
+            begin = System.currentTimeMillis();
+            MergeSortOriginal.sortIt(testArrOriginal,0,testArrOriginal.length-1);
+            end = System.currentTimeMillis();
+
+            total += (end - begin);
+        }
+
+        return total/ITER;
     }
 
-    public static long insertion(int[] testArrInsert){
-        long begin, end;
-        begin = System.currentTimeMillis();
-        InsertionSort.sortIt(testArrInsert,0,testArrInsert.length-1);
-        //end = System.currentTimeMillis();
-        end = System.currentTimeMillis();
-        return end-begin;
+    public static long insertion(int size){
+        int[] testArrInsert;
+        long total = 0;
+
+        for(int i = 0; i < ITER; i++){
+            testArrInsert = GenerateInput.generateRandom(size, MAX);
+            long begin, end;
+            begin = System.currentTimeMillis();
+            InsertionSort.sortIt(testArrInsert,0,testArrInsert.length-1);
+            end = System.currentTimeMillis();
+
+            total += (end - begin);
+        }
+
+        return total/ITER;
     }
 
-    public static long hybrid(int[] testArrHybrid){
-        long begin, end;
-        begin = System.currentTimeMillis();
-        HybridSort.sortIt(testArrHybrid,0,testArrHybrid.length-1);
-        //end = System.currentTimeMillis();
-        end = System.currentTimeMillis();
-        return end-begin;
+    public static long hybrid(int size){
+        int[] testArrHybrid = GenerateInput.generateRandom(size, MAX);
+        long total = 0;
+        
+        for(int i = 0; i < ITER; i++){
+            testArrHybrid = GenerateInput.generateRandom(size, MAX);
+            long begin, end;
+            begin = System.currentTimeMillis();
+            HybridSort.sortIt(testArrHybrid,0,testArrHybrid.length-1);
+            end = System.currentTimeMillis();
+
+            total += (end - begin);
+        }
+
+        return total/ITER;
     }
 
     public static void main(String[] args) {
-        int size = 10000, max = 100;
-        // 3 copies of the same array
-        int[] testArr1 = GenerateInput.generateRandom(size, max);
-        int[] testArr2 = GenerateInput.generateRandom(size, max);
-        int[] testArr3 = GenerateInput.generateRandom(size, max);
-        int[] testArr4 = GenerateInput.generateRandom(size, max);
+        int size = 10000;
 
         long mergeTime1, mergeTime2, insertTime, hybridTime;
 
-        mergeTime1 = mergeInplace(testArr1);
-        mergeTime2 = mergeOriginal(testArr2);
-        insertTime = insertion(testArr3);
-        hybridTime = hybrid(testArr4);
+        mergeTime1 = mergeInplace(size);
+        mergeTime2 = mergeOriginal(size);
+        insertTime = insertion(size);
+        hybridTime = hybrid(size);
 
-        System.out.println("Time for MergeSort(Inplace) : "+ mergeTime1 + " ms");
-        System.out.println("Time for MergeSort(Orignal) : "+ mergeTime2 + " ms");
-        System.out.println("Time for InsertionSort: "+ insertTime + " ms");
-        System.out.println("Time for HybridSort: "+ hybridTime + " ms");
-        System.out.println("\nHybridSort is "+ ((double)hybridTime/(double)mergeTime2) *100+ "% of the original MergeSort");
-        
+        System.out.println("Average Time for MergeSort(Inplace) over " + ITER + " runs : "+ mergeTime1 + " ms");
+        System.out.println("Avergae Time for MergeSort(Original) over " + ITER + " runs : "+ mergeTime2 + " ms");
+        System.out.println("Average Time for InsertionSort over " + ITER + " runs : "+ insertTime + " ms");
+        System.out.println("Average Time for HybridSort over " + ITER + " runs : "+ hybridTime + " ms");
+        System.out.println("\nHybridSort is "+ (hybridTime/mergeTime2)*100 + "% of the original MergeSort");
     }
-    
 }
