04b2c10efd51e493b8dfc3e37ee4187cbeaccbf4
ZERUI GONG <Nomlasssssss@outlook.com>
2022-09-10 15:27:32 +0800

    <modify> modified insertionsort (include start-end index in function head)

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -3,13 +3,12 @@ import java.util.*;
 public class InsertionSort {
     public static void main(String[] args) {
         int[] arr = {1,4,5,2,4,6,11,8,9};
-        sortIt(arr);
+        sortIt(arr,0, arr.length-1);
         System.out.println(Arrays.toString(arr));
         
     }
-    public static void sortIt(int[] arr){
-        int n = arr.length;
-        for(int i = 1; i<n; i++){
+    public static void sortIt(int[] arr, int start, int end) {
+        for(int i = start+1; i<=end; i++){
             for(int j = i; j>0; j--){
                 if(arr[j]<arr[j-1])
                     swap(arr, j,j-1);
