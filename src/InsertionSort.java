2a1a589c04f6a552054b6050c4546c82e1dc617f
ZERUI GONG <Nomlasssssss@outlook.com>
2022-09-12 23:25:15 +0800

    <modify> removed main

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -1,12 +1,5 @@
-import java.util.*;
 
 public class InsertionSort {
-    public static void main(String[] args) {
-        int[] arr = { 1, 4, 5, 2, 4, 6, 11, 8, 9 };
-        sortIt(arr, 0, arr.length - 1);
-        System.out.println(Arrays.toString(arr));
-
-    }
 
     public static void sortIt(int[] arr, int start, int end) {
         for (int i = start + 1; i <= end; i++) {
