2c9908b5887a3b34172fe2a47f048f19fe527b17
Joel David Lim <72362709+joedav98@users.noreply.github.com>
2022-09-12 12:30:43 +0800

    Several stopwatches tested

--- src/TimeCompare.java
+++ src/TimeCompare.java
@@ -1,21 +1,16 @@
 import java.io.*;
 
+// import org.apache.commons.lang3.time.StopWatch;
+import com.google.common.base.Stopwatch;
+import java.util.concurrent.TimeUnit;
+
 public class TimeCompare {
+
     static final int MAX = 100; // Max value of array elements
     static final int ITER = 1000; // Number of iterations for each test
-    static final int MAX_SIZE = 1000; // Max size of array
+    static final int MAX_SIZE = 200; // Max size of array
     static final int INCREMENT = 1; // Increment size for array
     static final int MODULO = 1; // Modulo for nanoTime
-
-    public static void CSVprinter(long[] wasp, String filename) throws IOException {
-        FileWriter writer = new FileWriter(filename);
-
-        for (int j = 0; j < wasp.length; j++) {
-            writer.append(String.valueOf(wasp[j]));
-            writer.append("\n");
-        }
-        writer.close();
-    }
 
     public static long mergeInplace(int size){
         int[] testArrInplace;
@@ -35,19 +30,26 @@ public class TimeCompare {
     }
 
     public static long mergeOriginal(int size){
+
         int[] testArrOriginal; 
         long total = 0;
-        
+
         for(int i = 0; i < ITER; i++){
             testArrOriginal = GenerateInput.generateRandom(size, MAX);
             long begin, end;
-            begin = System.nanoTime()/MODULO;
-            // begin = System.currentTimeMillis();
-            MergeSortOriginal.sortIt(testArrOriginal,0,testArrOriginal.length-1);
-            // end = System.currentTimeMillis();
-            end = System.nanoTime()/MODULO;
 
-            total += (end - begin);
+            Stopwatch stopWatch = Stopwatch.createStarted();
+            // begin = System.nanoTime()/MODULO;
+            // begin = System.currentTimeMillis();
+
+            MergeSortOriginal.sortIt(testArrOriginal,0,testArrOriginal.length-1);
+
+            stopWatch.stop();
+            // end = System.nanoTime()/MODULO;
+            // end = System.currentTimeMillis();
+
+            total += stopWatch.elapsed(TimeUnit.NANOSECONDS);
+            stopWatch.reset();
         }
 
         return total/ITER;
@@ -60,13 +62,19 @@ public class TimeCompare {
         for(int i = 0; i < ITER; i++){
             testArrInsert = GenerateInput.generateRandom(size, MAX);
             long begin, end;
-            // begin = System.currentTimeMillis();
-            begin = System.nanoTime()/MODULO;
-            InsertionSort.sortIt(testArrInsert,0,testArrInsert.length-1);
-            // end = System.currentTimeMillis();
-            end = System.nanoTime()/MODULO;
 
-            total += (end - begin);
+            Stopwatch stopWatch = Stopwatch.createStarted();
+            // begin = System.currentTimeMillis();
+            // begin = System.nanoTime()/MODULO;
+
+            InsertionSort.sortIt(testArrInsert,0,testArrInsert.length-1);
+
+            stopWatch.stop();
+            // end = System.currentTimeMillis();
+            // end = System.nanoTime()/MODULO;
+
+            total += stopWatch.elapsed(TimeUnit.NANOSECONDS);
+            stopWatch.reset();
         }
 
         return total/ITER;
@@ -93,28 +101,30 @@ public class TimeCompare {
         int count = 0;
         long mergeTime1, mergeTime2, insertTime, hybridTime;
 
-        long[] mergeTimes = new long[MAX_SIZE/INCREMENT + 1];
+        // mergeTime1 = mergeOriginal(1000);
 
-        for(int i = 1; i <= MAX_SIZE; i += INCREMENT){
+        long[] mergeTimes = new long[MAX_SIZE/INCREMENT];
+
+        for(int i = 2; i <= MAX_SIZE; i += INCREMENT){
             System.out.println("Iteration: " + count + ", Size: " + i);
             mergeTime1 = mergeOriginal(i);
             mergeTimes[count] = mergeTime1;
             count++;
         }
 
-        CSVprinter(mergeTimes, "merge.csv");
+        makeCSV.CSVprinter(mergeTimes, "merge.csv");
 
         count = 0;
-        long[] insertTimes = new long[MAX_SIZE/INCREMENT + 1];
+        long[] insertTimes = new long[MAX_SIZE/INCREMENT];
 
-        for(int i = 1; i <= MAX_SIZE; i += INCREMENT){
+        for(int i = 2; i <= MAX_SIZE; i += INCREMENT){
             System.out.println("Iteration: " + count + ", Size: " + i);
             insertTime = insertion(i);
             insertTimes[count] = insertTime;
             count++;
         }
         
-        CSVprinter(insertTimes, "insert.csv");
+        makeCSV.CSVprinter(insertTimes, "insert.csv");
 
         // mergeTime1 = mergeInplace(size);
         // mergeTime2 = mergeOriginal(size);
